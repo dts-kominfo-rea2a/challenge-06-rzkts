eb312a1d8d1577a778a4a882433907887a43b0f7
rzkts <rizkituls212@gmail.com>
2022-06-12 01:10:43 +0700

    JS Callback 2 menampilkan data dalam bentuk array of string

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,55 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  let result = [];
+
+  fs.readFile(
+    file1,
+    {
+      encoding: "utf8",
+    },
+    (err, data) => {
+      if (err) {
+        return fnCallBack(err, null);
+      } else {
+        const resultData = JSON.parse(data);
+        result.push(resultData.message.split(" ")[1]);
+      }
+    }
+  );
+
+  fs.readFile(
+    file2,
+    {
+      encoding: "utf8",
+    },
+    (err, data) => {
+      if (err) {
+        return fnCallBack(err, null);
+      } else {
+        const resultData = JSON.parse(data);
+        result.push(resultData[0].message.split(" ")[1]);
+      }
+    }
+  );
+
+  fs.readFile(
+    file3,
+    {
+      encoding: "utf8",
+    },
+    (err, data) => {
+      if (err) {
+        return fnCallBack(err, null);
+      } else {
+        const resultData = JSON.parse(data);
+        result.push(resultData[0].data.message.split(" ")[1]);
+        fnCallBack(null, result);
+      }
+    }
+  );
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
